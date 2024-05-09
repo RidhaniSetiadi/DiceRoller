c4ef443cf1e23e7e71ec0be6e6459c8ef5839f2d
RidhaniSetiadi <ridhanisetiadi08@gmail.com>
2024-05-09 20:00:11 +0800

    prak mobile

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.diceroller
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -13,18 +14,15 @@ class MainActivity : AppCompatActivity() {
 
         val rollButton: Button = findViewById(R.id.button)
         rollButton.setOnClickListener {
-            rollButton.setOnClickListener { rollDice() }
+            rollDice()
         }
-        rollDice()
     }
 
     private fun rollDice() {
-        val dice = Dice(6)
-        val diceRoll = dice.roll()
-
-        val diceImage: ImageView = findViewById(R.id.imageView2)
-
-        val drawableResource = when (diceRoll) {
+        val dice1 = Dice(6)
+        val diceRoll1 = dice1.roll()
+        val diceImage1: ImageView = findViewById(R.id.imageView2)
+        val drawableResource1 = when (diceRoll1) {
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
             3 -> R.drawable.dice_3
@@ -33,7 +31,29 @@ class MainActivity : AppCompatActivity() {
             else -> R.drawable.dice_6
         }
 
-        diceImage.setImageResource(drawableResource)
+        diceImage1.setImageResource(drawableResource1)
+
+        val dice2 = Dice(6)
+        val diceRoll2 = dice2.roll()
+        val diceImage2: ImageView = findViewById(R.id.imageView3)
+        val drawableResource2 = when (diceRoll2) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        diceImage2.setImageResource(drawableResource2)
+
+        if (diceRoll1 == diceRoll2){
+            val toast = Toast.makeText(this, "Selamat anda dapat dadu double!", Toast.LENGTH_SHORT)
+            toast.show() }
+
+        else {
+            val toast = Toast.makeText(this, "Anda belum beruntung!", Toast.LENGTH_SHORT)
+            toast.show() }
     }
 
     class Dice(private val numSides: Int) {
